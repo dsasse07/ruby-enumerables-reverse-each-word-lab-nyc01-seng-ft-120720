087b7aa2c755a5fddb15e071b20dc4e03ee2efce
Daniel Sasse <dsasse07@gmail.com>
2020-11-15 19:15:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,16 @@
-def reverse_each_word (sentence)
-  new_sentence = []
-  sentence_array = sentence.split
-  sentence_array.each do |word|
-    new_sentence << word.reverse
-  end
-  new_sentence.join(" ")
-end
+# def reverse_each_word (sentence)
+#   new_sentence = []
+#   sentence_array = sentence.split
+#   sentence_array.each do |word|
+#     new_sentence << word.reverse
+#   end
+#   new_sentence.join(" ")
+# end
 
-def reverse
+def reverse_each_word (sentence)
+  sentence_array = sentence.split
+  sentence_array.collect do |word|
+    word.reverse
+  end
+  sentence_array.join(" ")
+end
